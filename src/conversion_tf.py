8136768e5669f145afd0d171e6c379143f8dc2bb
Tobias Lotz <tobias.lotz@tu-dortmund.de>
2023-02-06 18:51:44 +0100

    implemented TTDescisionTreeImpl module in Tensorflow and tested correctness of tf module implementations

--- src/conversion_tf.py
+++ src/conversion_tf.py
@@ -6,6 +6,10 @@ TODO
 1. replace unsupported ops 
 2. address the loss of accuracy when using int8 instead of float32
 '''
+tf.config.run_functions_eagerly(True)
+
+BATCH_SIZE = 1
+
 class GEMMDecisionTreeImpl(tf.Module):
 
     def __init__(self, skl_model):
@@ -13,7 +17,7 @@ class GEMMDecisionTreeImpl(tf.Module):
         self.op = self.container.model._operators[0]
 
     # input signature shape (batch_size, n_features)
-    @tf.function(input_signature=[tf.TensorSpec(shape=(300, 8), dtype=tf.float32)])
+    @tf.function(input_signature=[tf.TensorSpec(shape=(BATCH_SIZE, 8), dtype=tf.float32)])
     def __call__(self, x, train=False):
         x = tf.transpose(x)
         
@@ -49,3 +53,63 @@ class GEMMDecisionTreeImpl(tf.Module):
         x = tf.transpose(tf.reduce_sum(x, 0))
 
         return tf.math.argmax(x, axis=1), x
+
+
+class TreeTraversalDecisionTreeImpl(tf.Module):
+    def _expand_indexes(self, batch_size):
+        indexes = self.op.nodes_offset
+        indexes = indexes.expand(batch_size, self.op.num_trees)
+        return indexes.detach().numpy().reshape(-1)
+    
+    def __init__(self, skl_model):
+        self.container = convert(skl_model, 'torch', extra_config={"tree_implementation":"tree_trav"})
+        self.op = self.container.model._operators[0]
+
+    # input signature shape (batch_size, n_features)
+    @tf.function(input_signature=[tf.TensorSpec(shape=(BATCH_SIZE, 8), dtype=tf.float32)])
+    def __call__(self, x):
+        #indexes = self._expand_indexes(tf.shape(x).numpy()[0])
+        indexes = self._expand_indexes(BATCH_SIZE)
+
+        decision_cond = tf.math.less_equal
+        if self.op.decision_cond.__name__ == 'le':
+            decision_cond = tf.math.less_equal
+        elif self.op.decision_cond.__name__ == 'ge':
+            decision_cond = tf.math.greater_equal
+        elif self.op.decision_cond.__name__ == 'lt':
+            decision_cond = tf.math.less
+        elif self.op.decision_cond.__name__ == 'gt':
+            decision_cond = tf.math.greater
+        elif self.op.decision_cond.__name__ == 'eq':
+            decision_cond = tf.math.equal
+        else:
+            decision_cond = tf.math.not_equal
+
+        for _ in range(self.op.max_tree_depth):
+            tree_nodes = indexes
+            feature_nodes = tf.reshape(tf.gather(self.op.features.detach(), axis=0, indices=tree_nodes), [-1, self.op.num_trees])
+            feature_values = tf.gather(x, feature_nodes, axis=1)
+
+            thresholds = tf.reshape(tf.gather(self.op.thresholds.detach(), indexes, axis=0), [-1, self.op.num_trees])
+            lefts = tf.reshape(tf.gather(self.op.lefts.detach(), indexes, axis=0), [-1, self.op.num_trees])
+            rights = tf.reshape(tf.gather(self.op.rights.detach(), indexes, axis=0), [-1, self.op.num_trees])
+
+            indexes = tf.cast(tf.where(decision_cond(feature_values, thresholds), lefts, rights), dtype=tf.int64)
+            indexes = indexes + self.op.nodes_offset.detach()
+            indexes = tf.reshape(indexes, [-1,])
+
+        output = tf.reshape(tf.gather(self.op.values.detach(), indexes,axis=0), [-1, self.op.num_trees, self.op.n_classes])
+
+        output = tf.reduce_sum(output, 1)
+
+        return tf.math.argmax(output, axis=1), output
+
+
+class PerfectTreeTraversalDecisionTreeImpl(tf.Module):
+    def __init__(self, skl_model):
+        self.container = convert(skl_model, 'torch', extra_config={"tree_implementation":"perf_tree_trav"})
+        self.op = self.container.model._operators[0]
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=(300, 8), dtype=tf.float32)])
+    def __call__(self, x):
+        pass
--- src/testing.py
+++ src/testing.py
@@ -1,14 +1,19 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 import tensorflow as tf
-from conversion_tf import GEMMDecisionTreeImpl
+from conversion_tf import GEMMDecisionTreeImpl, TreeTraversalDecisionTreeImpl
 import numpy as np
+from hummingbird.ml import convert
+import torch
+
+###################################### DEFINITIONS #################################################################
 
 def representative_dataset():
     for _ in range(100):
-      data = np.random.uniform(low=0., high=8., size=(300,8))
+      data = np.random.uniform(low=0., high=8., size=(1,8))
       yield [data.astype(np.float32)]
  
+tf.config.run_functions_eagerly(True)
 
 forest = RandomForestClassifier(n_estimators=105)
 X, y = make_classification(n_samples=1300, n_features=8,
@@ -21,54 +26,101 @@ x_test, y_test = X[1000:], y[1000:]
 
 forest.fit(x_train, y_train)
 
-
-model = GEMMDecisionTreeImpl(forest)
 X = tf.constant([1, 2, 3, 4, 5, 6, 7, 8], shape=[1, 8], dtype=tf.int8)
 X_float = tf.constant([1., 2., 3., 4., 5., 6., 7., 8.], shape=[1, 8])
 
-#y_pred, y = model(X_float)
 
-concrete_func = model.__call__.get_concrete_function()
+##################################### WORKING TFLITE FOR TT ############################################################
 
-converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func], model)
+conv_model = convert(forest, 'torch', extra_config={"tree_implementation":"tree_trav"})
+
+y_mod_pred_tt, y_mod_tt = conv_model.model._operators[0].forward((torch.tensor([[1., 2., 3., 4., 5., 6., 7., 8.]])))
+
+model_tt = TreeTraversalDecisionTreeImpl(forest)
+
+y_pred_tt, y_tt = model_tt(X_float)
+
+concrete_func = model_tt.__call__.get_concrete_function()
+
+converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func], model_tt)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.representative_dataset = representative_dataset
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
 converter.inference_input_type = tf.int8  
 converter.inference_output_type = tf.int8  
-tflite_model = converter.convert()
+tflite_model_tt = converter.convert()
 
-interpreter = tf.lite.Interpreter(model_content=tflite_model)
+interpreter = tf.lite.Interpreter(model_content=tflite_model_tt)
+interpreter.allocate_tensors()
+
+output = interpreter.get_output_details()[0]  
+input = interpreter.get_input_details()[0]  
+
+interpreter.set_tensor(input['index'], X)
+interpreter.invoke()
+y_lite_tt = interpreter.get_tensor(output['index'])
+
+y_pred_lite_tt = np.argmax(y_lite_tt, axis=1)
+
+
+###################################### WORKING TFLITE FOR GEMM ##########################################################
+
+model_gemm = GEMMDecisionTreeImpl(forest)
+
+conv_model = convert(forest, 'torch', extra_config={"tree_implementation":"gemm"})
+
+y_mod_pred_gemm, y_mod_gemm = conv_model.model._operators[0].forward((torch.tensor([[1., 2., 3., 4., 5., 6., 7., 8.]])))
+
+y_pred_gemm, y_gemm = model_gemm(X_float)
+
+concrete_func = model_gemm.__call__.get_concrete_function()
+
+converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func], model_gemm)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int8  
+converter.inference_output_type = tf.int8  
+tflite_model_gemm = converter.convert()
+
+interpreter = tf.lite.Interpreter(model_content=tflite_model_gemm)
 interpreter.allocate_tensors()
 
 output = interpreter.get_output_details()[0]  
 input = interpreter.get_input_details()[0]  
 
 
-#interpreter.set_tensor(input['index'], X)
-#interpreter.invoke()
-#y_lite = interpreter.get_tensor(output['index'])
-#
-#y_pred_lite = np.argmax(y_lite, axis=1)
-
-#print(f'Prediction     : {y_pred.numpy()[0]} \t Class Confidence: {y.numpy()[0]}')
-#print(f'Prediction lite: {y_pred_lite[0]} \t Class Confidence: {y_lite[0]}')
-
-preds, _ = model(x_test)
-diff = preds - y_test
-correct = diff[diff == 0].shape[0]
-total = diff.shape[0]
-
-interpreter.set_tensor(input['index'], x_test.astype(np.int8))
+interpreter.set_tensor(input['index'], X)
 interpreter.invoke()
-test_pred_quant = interpreter.get_tensor(output['index'])
+y_lite_gemm = interpreter.get_tensor(output['index'])
 
-preds_quant = np.argmax(test_pred_quant, axis=1)
+y_pred_lite_gemm = np.argmax(y_lite_gemm, axis=1)
 
-diff_quant = preds_quant - y_test
-correct_quant = diff_quant[diff_quant == 0].shape[0]
-total = diff_quant.shape[0]
+##################### ACCURACY TEST WITH BATCH SIZE 300 COMMENTED WHILE BATCH SIZE ABOVE IS 1 ###########################
+#diff = preds - y_test
+#correct = diff[diff == 0].shape[0]
+#total = diff.shape[0]
+#
+#interpreter.set_tensor(input['index'], x_test.astype(np.int8))
+#interpreter.invoke()
+#test_pred_quant = interpreter.get_tensor(output['index'])
+#
+#preds_quant = np.argmax(test_pred_quant, axis=1)
+#
+#diff_quant = preds_quant - y_test
+#correct_quant = diff_quant[diff_quant == 0].shape[0]
+#total = diff_quant.shape[0]
+#
+#print(f'Accuracy pre  quantization: {correct / total * 100 : .2f}%')
+#print(f'Accuracy post quantization: {correct_quant / total * 100 : .2f}%')
 
-print(f'Accuracy pre  quantization: {correct / total * 100 : .2f}%')
-print(f'Accuracy post quantization: {correct_quant / total * 100 : .2f}%')
 
+############################################ RESULTS ###############################################
+print(f'############################################ GEMM #############################################################\n')
+print(f'Prediction original    : {y_mod_pred_tt.detach().numpy()[0]} \t Class Confidence: {y_mod_tt.detach().numpy()[0]}')
+print(f'Prediction tf module   : {y_pred_tt.numpy()[0]} \t Class Confidence: {y_tt.numpy()[0]}')
+print(f'Prediction tflite quant: {y_pred_lite_tt[0]} \t Class Confidence: {y_lite_tt[0]}')
+print(f'\n############################################ TT ###############################################################\n')
+print(f'Prediction original    : {y_mod_pred_gemm.detach().numpy()[0]} \t Class Confidence: {y_mod_gemm.detach().numpy()[0]}')
+print(f'Prediction tf module   : {y_pred_gemm.numpy()[0]} \t Class Confidence: {y_gemm.numpy()[0]}')
+print(f'Prediction tflite quant: {y_pred_lite_gemm[0]} \t Class Confidence: {y_lite_gemm[0]}')
